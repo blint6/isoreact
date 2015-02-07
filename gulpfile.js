9a154e80248785e7a92f244439b17d6693fc4759
blint <blint@blint-station>
2015-02-07 15:02:39 +0100

    Restructure - no client/server architecture in the app. Only components should handle this.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var browserify = require('browserify');
 var gulp = require('gulp');
 var del = require('del');
 var insert = require('gulp-insert');
@@ -8,11 +7,7 @@ var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 var changed = require('gulp-changed');
 var watch = require('gulp-watch');
-var sourcemaps = require('gulp-sourcemaps');
 var to5 = require('gulp-6to5');
-var to5ify = require('6to5ify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
 
 var getBundleName = function () {
@@ -25,37 +20,6 @@ var paths = {
     js: 'src/**/*.js',
     asset: ['src/**', '!src/**/*.js']
 };
-
-gulp.task('javascript', function () {
-
-    var bundle = function () {
-        return browserify({
-                debug: true
-            })
-            .transform(to5ify.configure({
-                experimental: true
-            }))
-            .require('./src/client/start.js', {
-                entry: true
-            })
-            .bundle()
-            .on("error", function (err) {
-                console.log("Error: " + err.message);
-            })
-            .pipe(source(getBundleName() + '.js'))
-            .pipe(buffer())
-            .pipe(sourcemaps.init({
-                loadMaps: true
-            }))
-            // Add transformation tasks to the pipeline here.
-            //.pipe(uglify())
-            .pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest('./build/js/'));
-    };
-
-    return bundle();
-});
-
 
 gulp.task('clean', function (cb) {
     del.sync('build', {
--- src/client/start.js
+++
@@ -1,9 +0,0 @@
-import io from 'socket.io';
-import {
-    components, install
-}
-from '../component/componentinstaller';
-
-let socket = io('http://localhost');
-
-install((name => `../component/${name}/${name}.cli.js`), socket);
